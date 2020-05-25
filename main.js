836b4697b3459c95fbc48d1fe800b32539269bce
Antonio <antonioeduardo31211@gmail.com>
2020-05-25 20:47:49 -0300

    Primeiros passos

---
+++ main.js
@@ -0,0 +1,66 @@
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade?");//prompt exibe uma pergunta
+
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/
+
+/*
+var frutas = [{nome: "maçã", cor: "vermelha"},{nome: "uva", cor: "roxa"} ]
+console.log(frutas);
+alert(frutas[1]);
+/*
+
+/*
+var fruta = {nome: "maçã", cor: "vermelha"};
+console.log(fruta);
+*/
+
+/*
+var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva");
+//lista.pop();
+
+console.log(lista[2]);
+*/
+
+
+/*
+var nome = "Antonio Eduardo";
+var n1 = 29;
+var n2 = 10;
+var frase = "Isaac Newton foi uma pessoa importante para a ciência";
+//alert("Nome: " + nome + " tem " + n1 + " anos");
+//alert (n1 + n2);
+console.log(nome);
+console.log(n1+n2);
+//console.log(frase.replace("ciência", "astronomia"));
+//alert (frase.replace("ciência", "astronomia"));
+console.log(frase.toUpperCase());
+*/
